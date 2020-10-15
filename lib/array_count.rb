cd312a909148ce2defccac2dc201f059904e954c
Cassandra Whitty <cassandraallexwhitty@gmail.com>
2020-10-15 20:33:42 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,9 +1,10 @@
 def count_strings(array)
-  array.count do |element|
-    element.any?(String)
+  array.count do |object|
+    object.is_a?(String)
   end
   # Return the total number of strings in the provided array using the count enumerable
 end
+
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
